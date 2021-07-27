fca02c03cae7c55f7056b9ea4407c43251a54866
tongnamuu <thban33@naver.com>
2021-07-27 23:54:36 +0900

    1. Setup Typescript

---
+++ src/1.js
@@ -0,0 +1,4 @@
+"use strict";
+var v1 = 123;
+var v2 = function () { return console.log('123'); };
+console.log("typescript 실행");
---
+++ src/1.ts
@@ -0,0 +1,4 @@
+const v1: number = 123;
+const v2 = () => console.log('123');
+
+console.log("typescript 실행");
